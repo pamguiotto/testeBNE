5dbb1521dd0d0e3a31f96734f4966e8d3cd60d3b
Pamela Guiotto <pamguiotto@hotmail.com>
2022-03-22 15:00:39 -0300

    Atividade finalizada

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Teste_BNE
 {
@@ -14,6 +16,7 @@ namespace Teste_BNE
             Atd3();
             Atd4();
             Atd5();
+
 
         }
 
@@ -32,7 +35,8 @@ namespace Teste_BNE
             var alturaDosAndares = Convert.ToDouble(Console.ReadLine());
 
             var resultado = (quantidadeDeAndares * alturaDosAndares);
-            Console.WriteLine("Tamanho do edifíco = {0}", resultado);
+            Console.WriteLine("Tamanho do edifíco = {0}\n", resultado);
+            Console.WriteLine("----------------\n");
 
         }
 
@@ -41,14 +45,65 @@ namespace Teste_BNE
         /// </summary>
         static void Atd2()
         {
+            Console.WriteLine("Atividade 2");
+            Console.WriteLine("Identificando quantidades.\n");
+
+            Console.WriteLine("Digite o primeiro número:");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro número:");
+            var num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o quarto número:");
+            var num4 = Convert.ToInt32(Console.ReadLine());
+
+            int[] listaNum = { num1, num2, num3, num4 };
+
+
+            var menorNumero = listaNum.Min();
+            var maiorNumero = listaNum.Max();
+            var mediaDosNumeros = (listaNum.Sum() / 4);
+            Console.WriteLine("Menor número = {0}", menorNumero);
+            Console.WriteLine("Maior número = {0}", maiorNumero);
+            Console.WriteLine("Média dos números = {0}", mediaDosNumeros);
+            Console.WriteLine("----------------\n");
 
         }
 
         /// <summary>
-        /// Construa um algoritmo que instancia um array de inteiros com os seguintes valores: 1,3,35,40,85,123,121,209,200,305,350.Após, crie uma lógica que separe estes números entre pares e ímpares em outros 2 arrays.
+        /// Construa um algoritmo que instancia um array de inteiros com os seguintes valores: 1,3,35,40,85,123,121,209,200,305,350. Após, crie uma lógica que separe estes números entre pares e ímpares em outros 2 arrays.
         /// </summary>
         static void Atd3()
         {
+            Console.WriteLine("Atividade 3");
+            Console.WriteLine("Separando pares e impares.\n");
+            Console.WriteLine("Click para dividir em dois arrays estes valores: 1,3,35,40,85,123,121,209,200,305,350");
+            int[] listaOriginal = { 1, 3, 35, 40, 85, 123, 121, 209, 200, 305, 350 };
+
+            var listaPar = new List<int>();
+            var listaImpar = new List<int>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var numeroListaOrinal = listaOriginal[i];
+                 
+                if (numeroListaOrinal % 2 != 0)
+                {
+                    listaImpar.Add(numeroListaOrinal);
+                }
+                else
+                {
+                    listaPar.Add(numeroListaOrinal);
+                }
+            }
+            Console.ReadLine();
+            Console.WriteLine("Lista Original: {0}", string.Join(",", listaOriginal));
+            Console.WriteLine("Lista de Pares: {0}", string.Join(",", listaPar));
+            Console.WriteLine("Lista de Impares: {0}", string.Join(",", listaImpar));
+            Console.WriteLine("----------------\n");
 
         }
 
@@ -57,6 +112,33 @@ namespace Teste_BNE
         /// </summary>
         static void Atd4()
         {
+            Console.WriteLine("Atividade 4");
+            Console.WriteLine("Ordenar números.\n");
+            Console.WriteLine("Digite o primeiro número:");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro número:");
+            var num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o quarto número:");
+            var num4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o quinto número:");
+            var num5 = Convert.ToInt32(Console.ReadLine());
+
+            int[] listaNum = { num1, num2, num3, num4, num5 };
+
+            //ordena os números aqui
+            Array.Sort(listaNum);
+            Array.Reverse(listaNum);
+
+            Console.WriteLine("Lista ordenadas: {0}", string.Join(",", listaNum));
+
+            Console.WriteLine("----------------\n");
+
 
         }
 
@@ -65,7 +147,16 @@ namespace Teste_BNE
         /// </summary>
         static void Atd5()
         {
+            Console.WriteLine("Atividade 5");
+            Console.WriteLine("Contar letras.\n");
+            Console.WriteLine("digite uma frase:");
+            var frase = Console.ReadLine();
 
+            var numeroDeVogais = frase.Count(x => (x == 'a') || (x == 'e') || (x == 'i') || (x == 'o') || (x == 'u'));
+
+            Console.WriteLine("numero de vogais={0}", numeroDeVogais);
+            Console.WriteLine("OBRIGADO, ATÉ LOGO!.\n");
+            Console.WriteLine("----------------\n");
         }
     }
 }
